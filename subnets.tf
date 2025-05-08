0417426e4e802e89b04a48b56c89606efb78aa18
Arjun <arjunc2@gmail.com>
2025-05-08 20:26:06 +0530

    final commit

--- subnets.tf
+++ subnets.tf
@@ -1,19 +1,19 @@
 resource "aws_subnet" "public_subnets" {
-    count = "${length(var.public_cidrs)}"
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${element(var.public_cidrs,count.index)}"
-    availability_zone = "${element(var.azs,count.index)}"
-    tags = {
-        Name = "${var.vpc_name}-Public-Subnet-${count.index+1}"
-    }
+  count             = length(var.public_cidrs)
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = element(var.public_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  tags = {
+    Name = "${var.vpc_name}-Public-Subnet-${count.index + 1}"
+  }
 }
 
 resource "aws_subnet" "private_subnets" {
-    count = "${length(var.private_cidrs)}"
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${element(var.private_cidrs,count.index)}"
-    availability_zone = "${element(var.azs,count.index)}"
-    tags = {
-        Name = "${var.vpc_name}-Private-Subnet-${count.index+1}"
-    }
+  count             = length(var.private_cidrs)
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = element(var.private_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  tags = {
+    Name = "${var.vpc_name}-Private-Subnet-${count.index + 1}"
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,15 @@
 resource "aws_vpc" "default" {
-    cidr_block = "${var.vpc_cidr}"
-    instance_tenancy = "default"
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.vpc_name}"
-    }
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.vpc_name}"
+  }
 }
 
 resource "aws_internet_gateway" "IGW" {
-    vpc_id = "${aws_vpc.default.id}"
-    tags = {
-        Name = "${var.igw_name}"
-    }
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "${var.igw_name}"
+  }
 }
