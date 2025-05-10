65de347597f90993b308773d1be9062c39bf0bbe
Arjun <arjunc2@gmail.com>
2025-05-10 14:40:03 +0530

    added s3 backend

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "nagtfbackends3tfstate"
-#     key    = "path/to/my/key"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "nagtfbackends3tfstate"
+    key    = "path/to/my/key"
+  }
+}
